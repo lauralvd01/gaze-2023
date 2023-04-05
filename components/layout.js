6e593731e718a496b5b1ba3ce75dad7fd7e310bb
paulminesnancy <paulc5830@gmail.com>
2023-04-05 10:42:59 +0200

    style avance

--- components/layout.js
+++ components/layout.js
@@ -21,8 +21,8 @@ export default function Layout({children}){
             <header>
                 <nav className="navbar navbar-expand-lg bg-gradient shadow">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">
-                            <Image src="/../public/images/artem.png" alt="PA=anus" width="50" height="50"/>
+                        <a className="navbar-brand pb-2" href="/">
+                            <Image src="/../public/biere-petite.png" alt="PA=anus" width="50" height="50"/>
                         </a>
                         <a className="navbar-brand" href="/">GazeEDIfication</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -35,13 +35,7 @@ export default function Layout({children}){
                                 </li>
                             </ul>
                         </div>
-                        <div>
-                            <a className="btn" href="https://www.linkedin.com/in/paul-cambon54/">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
-                                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-                                </svg>
-                            </a>
-                        </div>
+                        <button type="button" class="btn btn-outline">Primary</button>
                     </div>
                 </nav>
             </header>
