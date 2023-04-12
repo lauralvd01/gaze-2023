a26343619da8e57d3c8b931b662a945d8665dad9
PALequeu <pierreantoinelequeu@gmail.com>
2023-04-12 12:30:18 +0200

    test commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '@/styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.css';
-
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
---
+++ pages/auth.js
@@ -0,0 +1,45 @@
+import React from "react";
+import supabase from "../lib/supabaseClient";
+import { useState } from "react";
+
+async function signInWithEmail() {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: "example@email.com",
+    password: "example-password",
+  });
+}
+
+function auth(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function signUpWithEmail() {
+    console.log("signUpWithEmail", email, password);
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+  }
+
+  return (
+    <div>
+      Test
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={signUpWithEmail}>Sign up with email</button>
+      <text>
+        {email} {password}
+      </text>
+    </div>
+  );
+}
+
+export default auth;
