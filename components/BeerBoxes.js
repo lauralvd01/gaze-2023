d35e61d5182d84aac15c7de6a9dc67a7df3bd3c9
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2023-05-30 08:59:12 +0200

    Avancées

--- components/BeerBoxes.js
+++ components/BeerBoxes.js
@@ -3,7 +3,7 @@ const BeerBoxes = ({beers}) => {
         <div className="card-group row row-cols-4 g-4">
             {beers.map((beer) => 
                 <div className="col" key={beer.id}>
-                <div className="card bg-warning" key={beer.id}>
+                <div className="card bg-warning border-dark" key={beer.id}>
                     {/*<img src="..." className="card-img-top" alt=""/>*/}
                     <div className="card-body">
                         <h5 className="card-title">{beer.name}</h5>
@@ -11,6 +11,7 @@ const BeerBoxes = ({beers}) => {
                     <div className="card-footer">
                         <p className="card-text">Degré : {beer.degree}</p>
                         <p className="card-text">Prix : {beer.price} €</p>
+                        <p className="card-text">Prix / Degré : {Math.floor(100*beer.price/beer.degree)/100} €</p>
                     </div>
                 </div>
                 </div>
