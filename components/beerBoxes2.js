5e793f6bd771748c13becf0c59c0ff8e6c7cf83d
paulminesnancy <paulc5830@gmail.com>
2023-06-07 10:43:48 +0200

    beerboxes2 et leaderboard

---
+++ components/beerBoxes2.js
@@ -0,0 +1,97 @@
+import Image from "next/image";
+import FormModal from "./FormModal";
+import { useState } from "react";
+import Form_v2 from "@/pages/form_v2.js";
+import { handleSave } from "@/pages/index.js";
+
+
+
+export default function BeerBoxes2 ({beers, prefilledBeer, setPrefilledBeer, userId}) {
+    const [openModal, setOpenModal] = useState(false);
+    return (
+        <div className="container">
+            <div className="row beer_row">
+                {beers.map((beer) =>(
+                        <div className="col col-lg-2 col-6">
+                            <div className="card beer_card">
+                                <div className="card-header">
+                                    <div className="card-img-top beer_img">
+                                        <MyImage id={beer.id} />
+                                    </div>
+                                    <h5 className="card-title">{beer.name}</h5>
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">Degré : {beer.degree}</p>
+                                    <p className="card-text">Prix : {beer.price} €</p>
+                                    <p className=""card-text>
+                                        Quantité : {beer.litrage}
+                                    </p>
+                                    <p className="card-text">
+                                        Prix / Degré.L :{" "}
+                                        {Math.floor((10000 * beer.price) / (beer.degree*beer.litrage)) / 100} €
+                                    </p>
+                                </div>
+                                <div className="card-footer">
+                                    <button
+                                        className="btn beer_button m-2"
+                                        onClick={() => {
+                                        if (openModal) {
+                                            setPrefilledBeer(null);
+                                        }
+                                        setPrefilledBeer(beer.name);
+                                        setOpenModal(!openModal);
+                                        }}
+                                    >
+                                        Boire
+                                    </button>
+                                    <FormModal
+                                        isOpen={openModal}
+                                        onClose={() => {
+                                        setOpenModal(false);
+                                        if (openModal) {
+                                            setPrefilledBeer(null);
+                                        }
+                                        }}
+                                    >
+                                        <FormModal.Header>Nouvelle consommation</FormModal.Header>
+                                        <FormModal.Body>
+                                        {/* Modal Body */}
+                                        <Form_v2 prefilledBeer={prefilledBeer} />{" "}
+                                        {/* Use Form to see previous version */}
+                                        </FormModal.Body>
+                                        <FormModal.Footer>
+                                        <FormModal.DismissButton className="modal-button-close">
+                                            Annuler
+                                        </FormModal.DismissButton>
+                                        <button
+                                            className="modal-button-save"
+                                            onClick={async () => {
+                                            await handleSave(userId);
+                                            setOpenModal(false);
+                                            }}
+                                        >
+                                            Sauvegarder
+                                        </button>
+                                        </FormModal.Footer>
+                                    </FormModal> 
+                                </div>
+                            </div>
+                        </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+const MyImage = ({ id }) => {
+    let impath = "/../public/beers/".concat(id).concat(".png");
+    return (
+      <Image
+        src={impath}
+        className="beer_image"
+        alt={impath}
+        width="120"
+        height="120"
+      />
+    );
+  };
--- components/leaderboard.js
+++ components/leaderboard.js
@@ -1,7 +1,20 @@
 import Image from "next/image";
-
+import ComputeCurrentDegree from "@/usefultools/ComputeCurrentDegrees";
+import { useState, useEffect } from "react";
 
 export default function Leaderboard({leader_list}){
+    const [degrees, setDegrees] = useState([]);
+
+    useEffect(() => {
+      const LogDegrees = async () => {
+        const degrees = await ComputeCurrentDegree();
+        setDegrees(degrees);
+      };
+      LogDegrees();
+      console.log("degrees successfully set");
+      console.log("degrees,", degrees);
+    }, []);
+
     return(
         <div className="leader_box">
             <h1>Le classement des champions :</h1>
@@ -51,3 +64,35 @@ export default function Leaderboard({leader_list}){
         </div>
     )
 }
+
+
+
+// const Leaderboard = () => {
+//   const [degrees, setDegrees] = useState([]);
+
+//   useEffect(() => {
+//     const LogDegrees = async () => {
+//       const degrees = await ComputeCurrentDegree();
+//       setDegrees(degrees);
+//     };
+//     LogDegrees();
+//     console.log("degrees successfully set");
+//     console.log("degrees,", degrees);
+//   }, []);
+
+//   return (
+//     <div style={{ backgroundColor: "#cdb4c4" }}>
+//       {degrees == []
+//         ? "Fetching degrees ..."
+//         : degrees.map((degree) => {
+//             return (
+//               <div key={degree.username}>
+//                 {degree.username} : {degree.degree}
+//               </div>
+//             );
+//           })}
+//     </div>
+//   );
+// };
+
+// export default Leaderboard;
