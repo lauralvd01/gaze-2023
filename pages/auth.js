4daa1a9e71bbbbee7ad04fd5af21b87ffa1cce00
paulminesnancy <paulc5830@gmail.com>
2023-06-08 16:15:13 +0200

    lib enlevé de gitignore

--- pages/auth.js
+++ pages/auth.js
@@ -1,5 +1,5 @@
 import React from "react";
-import supabase from "../lib/supabaseClient.js";
+import supabase from "../lib/supabaseClient";
 import { useState } from "react";
 
 async function signInWithEmail() {
